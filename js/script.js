06fe4ce56c6fa083856866765d72c80e66529f0f
Thomas Szentirmay <tlszent@outlook.com>
2023-07-21 00:26:37 -0400

    added styles and removed dummy text

--- js/script.js
+++ js/script.js
@@ -4,20 +4,20 @@ const sr = ScrollReveal({
 	reset: true
 });
 
-sr.reveal('.logo', { delay: 210, origin: 'left' });
+sr.reveal('.logo', { delay: 310, origin: 'left' });
 sr.reveal('.navlist', { delay: 310, origin: 'top' });
-sr.reveal('.top-btnn', { delay: 410, origin: 'right' });
+sr.reveal('.top-btnn', { delay: 310, origin: 'right' });
 
-sr.reveal('.main-content h4', { delay: 510, origin: 'top' });
-sr.reveal('.main-content h1', { delay: 610, origin: 'left' });
-sr.reveal('.main-content p', { delay: 710, origin: 'right' });
-sr.reveal('.social', { delay: 810, origin: 'left' });
-sr.reveal('.main-btnn', { delay: 910, origin: 'top' });
+sr.reveal('.main-content h4', { delay: 310, origin: 'top' });
+sr.reveal('.main-content h1', { delay: 310, origin: 'left' });
+sr.reveal('.main-content p', { delay: 310, origin: 'right' });
+sr.reveal('.social', { delay: 310, origin: 'left' });
+sr.reveal('.main-btnn', { delay: 310, origin: 'top' });
 
-sr.reveal('.bottom', { delay: 1010, origin: 'bottom' });
+sr.reveal('.bottom', { delay: 510, origin: 'bottom' });
 
 // Reveal the headers in .about, .services, .portfolio, and .contact sections
-sr.reveal('.about h2', { delay: 510, origin: 'left' });
-sr.reveal('.services h2', { delay: 510, origin: 'top' });
-sr.reveal('.portfolio h2', { delay: 510, origin: 'right' });
-sr.reveal('.contact h2', { delay: 510, origin: 'top' });
+sr.reveal('.about h2', { delay: 250, origin: 'left' });
+sr.reveal('.services h2', { delay: 250, origin: 'top' });
+sr.reveal('.portfolio h2', { delay: 250, origin: 'right' });
+sr.reveal('.contact h2', { delay: 250, origin: 'top' });
