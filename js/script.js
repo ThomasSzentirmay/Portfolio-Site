34e5b41c42dbfe512546f4ef9a45af8de4f0c10d
ThomasSzentirmay <132217664+ThomasSzentirmay@users.noreply.github.com>
2023-07-22 23:50:44 -0400

    made new image for mixmate project

--- js/script.js
+++ js/script.js
@@ -6,3 +6,22 @@ window.addEventListener("scroll", function () {
     header.classList.remove("header-scrolled");
   }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const navLinks = document.querySelectorAll(".navlist a");
+  navLinks.forEach(link => {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+      const targetSection = document.querySelector(link.getAttribute("href"));
+      if (targetSection) {
+        const targetOffset = targetSection.getBoundingClientRect().top;
+        const headerOffset = document.querySelector("header").offsetHeight;
+        const scrollOffset = targetOffset + window.scrollY - headerOffset;
+        window.scrollTo({
+          top: scrollOffset,
+          behavior: "smooth"
+        });
+      }
+    });
+  });
+});
